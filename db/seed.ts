a81a7e12070cd8e736697d8531045fcce2de545a
Adam Chlebek <adamchlebek@live.com>
2024-05-06 16:00:12 -0600

    Remove DB

--- db/seed.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "astro:db";
-
-export default async function seed() {
-  // Seed local development data.
-  // See https://docs.astro.build/en/guides/astro-db/#seed-your-database
-}
